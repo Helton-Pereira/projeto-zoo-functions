7cce9f2be8c3bd487e5848a9f4c198715b4b87a7
Helton Pereira <helton.bfsm@gmail.com>
2022-06-29 16:23:01 -0300

    Requisito 1 finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => species.find((specie) => specie.id === id));
 }
+
+console.log(species)
 
 module.exports = getSpeciesByIds;
