239a499541a187ef88b44d8ffa593259a72f72c4
Helton Pereira <helton.bfsm@gmail.com>
2022-06-29 17:49:22 -0300

    Requisito 3 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.filter((specie) => specie.name === animal).every((element) => {
+    return element.residents.age >= age;
+  });
 }
 
+// console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
