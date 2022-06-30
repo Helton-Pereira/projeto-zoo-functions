182b4eb159d4aa9741d80ab9f8d575da2b149370
Helton Pereira <helton.bfsm@gmail.com>
2022-06-30 18:29:50 -0300

    Requisito 6 cumprido(?)

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,16 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Returna Undefined caso nenhum parâmetro seja informado', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+  it('Retorna a mensagem "Parâmetro inválido, é necessário uma string" caso o parâmetro não seja uma string', () => {
+    expect(handlerElephants(5)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Retorna quantidade de elefantes quando o parâmetro for "count"', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Retorna um array com a relação dos nomes de todos os elefantes quando', () => {
+    expect(handlerElephants('name')).toEqual(['Ilana,', 'Orval', 'Bea', 'Jefferson']);
+  });
+});
