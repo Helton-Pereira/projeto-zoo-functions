98d4a085bb005a7cd0e81376686cc41dc860bbc4
Helton Pereira <helton.bfsm@gmail.com>
2022-06-30 17:36:28 -0300

    Requisitos 4 e 5 cumpridos

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const obj = {};
+  if (animal === undefined) {
+    species.forEach((element) => { obj[element.name] = element.residents.length; });
+    return obj;
+  } const result = species.find((specie) => specie.name === animal.specie);
+  if (animal.sex === undefined) {
+    return result.residents.length;
+  } const resultSex = result.residents.filter((resident) => resident.sex === animal.sex);
+  return resultSex.length;
 }
 
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 module.exports = countAnimals;
