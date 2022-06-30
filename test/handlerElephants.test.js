7671a2c5438f72404d1b7bf3cb55d93a17ebdbad
Helton Pereira <helton.bfsm@gmail.com>
2022-06-30 18:54:03 -0300

    Requisito 7 cumprido

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -10,7 +10,16 @@ describe('Testes da função HandlerElephants', () => {
   it('Retorna quantidade de elefantes quando o parâmetro for "count"', () => {
     expect(handlerElephants('count')).toBe(4);
   });
-  it('Retorna um array com a relação dos nomes de todos os elefantes quando', () => {
-    expect(handlerElephants('name')).toEqual(['Ilana,', 'Orval', 'Bea', 'Jefferson']);
+  it('Retorna um array com a relação dos nomes de todos os elefantes quando o parâmetro for "name"', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana,', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Retorna a média de idade dos elefantes quando o parâmetro for "averageAge"', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('Retorna a localização dos elefantes dentro do Zoológico quando o parâmetro for "location"', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+  it('Retorna a popularidade dos elefantes quando o parâmetro for "popularity"', () => {
+    expect(handlerElephants('popularity')).toBe(5);
   });
 });
